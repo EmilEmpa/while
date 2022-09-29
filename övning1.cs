30c22ad0c023ecca0271a401a02fd5848137d8f2
Emil Ackerot <emil.ackerot@gmail.com>
2022-09-29 13:22:13 +0200

    Jag har fixat så den räknar varje gissning.

--- övning1.cs
+++ övning1.cs
@@ -4,12 +4,14 @@ int slumptal = slump.Next(1, 100);
 
 
 int gissa = 0;
+int antalGissningar = 0;
+
 while (slumptal != gissa)
 {
     Console.WriteLine("Gissa ett tal mellan 1 och 100: ");
     gissa = int.Parse(Console.ReadLine());
-    int antalGissningar = 0;
     antalGissningar++;
+
     if (slumptal > gissa)
     {
         Console.WriteLine("För lågt!");
@@ -21,6 +23,6 @@ while (slumptal != gissa)
 
     else if (slumptal == gissa)
     {
-        Console.WriteLine("Grattis, Rätt tal! Det tog dig " + antalGissningar + " antal gissningar");
+        Console.WriteLine("Grattis, Rätt tal! Det tog dig " + antalGissningar.ToString() + " antal gissningar");
     }
 }
